ffcfb07da379707ece7a01bb3a6d0ae0b902544e
Arnaud Roland <arnaud.roland.ar@gmail.com>
2022-10-25 13:44:07 +0200

    all: add support for spm

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "BatchFirebaseDispatcher",
+    defaultLocalization: "en",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "BatchFirebaseDispatcher",
+            targets: ["BatchFirebaseDispatcher"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/BatchLabs/Batch-iOS-SDK", from: "1.19.0"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk", "8.10.0"..."10.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "BatchFirebaseDispatcher",
+            dependencies: [
+                .product(name: "FirebaseAnalytics", package: "firebase-ios-sdk"),
+            ],
+            path: "BatchFirebaseDispatcher",
+            publicHeadersPath: "Classes/"),
+        .testTarget(
+            name: "BatchFirebaseDispatcherTests",
+            dependencies: ["BatchFirebaseDispatcher"],
+            path: "BatchFirebaseDispatcherTests"
+        ),
+    ]
+)
