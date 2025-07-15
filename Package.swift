fa809a9283a410f4e71ff8c1a110eba21a79d338
Julien Di Marco <julien.dimarco@goandup.fr>
2025-07-15 17:31:50 +0200

    build: upgrade dependencies to '3.0.1' and platform to 'iOS 15'

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 let package = Package(
     name: "BatchFirebaseDispatcher",
     defaultLocalization: "en",
+    platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -13,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/BatchLabs/Batch-iOS-SDK","1.19.0"..."99.0.0"),
+        .package(url: "https://github.com/BatchLabs/Batch-iOS-SDK","3.0.1"..."99.0.0"),
         .package(url: "https://github.com/firebase/firebase-ios-sdk", "8.10.0"..."99.0.0"),
     ],
     targets: [
@@ -22,6 +23,7 @@ let package = Package(
         .target(
             name: "BatchFirebaseDispatcher",
             dependencies: [
+                .product(name: "Batch", package: "Batch-iOS-SDK"),
                 .product(name: "FirebaseAnalytics", package: "firebase-ios-sdk"),
             ],
             path: "BatchFirebaseDispatcher",
